766366bcd0dd8468681249b401f82f6871209876
Mengru Li <clarisseli@outlook.com>
2025-05-13 15:21:01 -0700

    Finish Assignment 1

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -1,7 +1,9 @@
 export default function Lab1() {
     return (
         <div id="wd-lab1">
+            <p><strong>Mengru Li (Clarisse)</strong> — Section 04</p>
             <h2>Lab 1</h2>
+
             <h3>HTML Examples</h3>
 
             <div id="wd-h-tag">
