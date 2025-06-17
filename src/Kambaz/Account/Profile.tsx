1d018bf41cdebd2fe36c0e362c657f522f198c97
Mengru Li <clarisseli@outlook.com>
2025-06-17 01:13:54 -0700

    updated profile

--- src/Kambaz/Account/Profile.tsx
+++ src/Kambaz/Account/Profile.tsx
@@ -1,9 +1,9 @@
-import * as client from "./client";
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { setCurrentUser } from "./reducer";
 import { FormControl, Button } from "react-bootstrap";
+import * as client from "./client";
 
 export default function Profile() {
     const [profile, setProfile] = useState<any>({});
@@ -11,12 +11,15 @@ export default function Profile() {
     const navigate = useNavigate();
     const { currentUser } = useSelector((state: any) => state.accountReducer);
     const updateProfile = async () => {
-        const status = await client.updateUser(profile);
-        dispatch(setCurrentUser(profile));
+        const updatedProfile = await client.updateUser(profile);
+        dispatch(setCurrentUser(updatedProfile));
+        await fetchProfile();
     };
-    const fetchProfile = () => {
+
+    const fetchProfile = async () => {
         if (!currentUser) return navigate("/Kambaz/Account/Signin");
-        setProfile(currentUser);
+        const profile = await client.profile();
+        setProfile(profile);
     };
     const signout = async () => {
         await client.signout();
@@ -33,22 +36,20 @@ export default function Profile() {
                         onChange={(e) => setProfile({ ...profile, username: e.target.value })} />
                     <FormControl defaultValue={profile.password} id="wd-password" className="mb-2"
                         onChange={(e) => setProfile({ ...profile, password: e.target.value })} />
-                    <FormControl defaultValue={profile.firstName} id="wd-firstname" className="mb-2"
+                    <FormControl defaultValue={profile.firstName} id="wd-firstname" className="mb-2" placeholder="First Name"
                         onChange={(e) => setProfile({ ...profile, firstName: e.target.value })} />
-                    <FormControl defaultValue={profile.lastName} id="wd-lastname" className="mb-2"
+                    <FormControl defaultValue={profile.lastName} id="wd-lastname" className="mb-2" placeholder="Last Name"
                         onChange={(e) => setProfile({ ...profile, lastName: e.target.value })} />
                     <FormControl defaultValue={profile.dob} id="wd-dob" className="mb-2"
                         onChange={(e) => setProfile({ ...profile, dob: e.target.value })} type="date" />
-                    <FormControl defaultValue={profile.email} id="wd-email" className="mb-2"
+                    <FormControl defaultValue={profile.email} id="wd-email" className="mb-2" placeholder="Email Address"
                         onChange={(e) => setProfile({ ...profile, email: e.target.value })} />
                     <select onChange={(e) => setProfile({ ...profile, role: e.target.value })}
-                        className="form-control mb-2" id="wd-role" value={profile.role || "USER"} >
+                        className="form-control mb-2" id="wd-role">
                         <option value="USER">User</option>            <option value="ADMIN">Admin</option>
                         <option value="FACULTY">Faculty</option>      <option value="STUDENT">Student</option>
                     </select>
-                    <Button onClick={updateProfile} className="btn btn-primary w-100 mb-2">
-                        Update
-                    </Button>
+                    <button onClick={updateProfile} className="btn btn-primary w-100 mb-2"> Update </button>
                     <Button onClick={signout} className="w-100 mb-2" id="wd-signout-btn">
                         Sign out
                     </Button>
