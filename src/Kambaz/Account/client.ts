9061b81376d97f7f2a3af80df4417c52d884fa1b
Mengru Li <clarisseli@outlook.com>
2025-06-17 21:09:01 -0700

    updated assignment

--- src/Kambaz/Account/client.ts
+++ src/Kambaz/Account/client.ts
@@ -44,10 +44,6 @@ export const findMyCourses = async () => {
     const { data } = await axiosWithCredentials.get(`${USERS_API}/current/courses`);
     return data;
 }
-export const findCoursesForUser = async (userId: string) => {
-    const response = await axiosWithCredentials.get(`${USERS_API}/${userId}/courses`);
-    return response.data;
-};
 export const createCourse = async (course: any) => {
     const { data } = await axiosWithCredentials.post(`${USERS_API}/current/courses`, course);
     return data;
@@ -60,17 +56,6 @@ export const createUser = async (user: any) => {
     const response = await axios.post(`${USERS_API}`, user);
     return response.data;
 };
-<<<<<<< HEAD
-export const enrollIntoCourse = async (userId: string, courseId: string) => {
-    const response = await axiosWithCredentials.post(`${USERS_API}/${userId}/courses/${courseId}`);
-    return response.data;
-};
-export const unenrollFromCourse = async (userId: string, courseId: string) => {
-    const response = await axiosWithCredentials.delete(`${USERS_API}/${userId}/courses/${courseId}`);
-    return response.data;
-};
-
-=======
 export const findCoursesForUser = async (userId: string) => {
     const response = await axiosWithCredentials.get(`${USERS_API}/${userId}/courses`);
     return response.data;
@@ -83,7 +68,5 @@ export const unenrollFromCourse = async (userId: string, courseId: string) => {
     const response = await axiosWithCredentials.delete(`${REMOTE_SERVER}/api/enrollments/${userId}/${courseId}`);
     return response.data;
 };
->>>>>>> a6
-
 
 
